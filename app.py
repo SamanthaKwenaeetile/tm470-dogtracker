2566657a8f16a2f07a0af3ed03ff59cfe157a64a
Samantha Kwenaeetile <74901613+SamanthaKwenaeetile@users.noreply.github.com>
2026-05-21 14:23:23 +0100

    Added SQLite database setup and the dog table also

--- app.py
+++ app.py
@@ -1,18 +1,42 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
+# Create database table
+def init_db():
+
+    connection = sqlite3.connect('database/dogtracker.db')
+
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS dogs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            breed TEXT NOT NULL,
+            age INTEGER NOT NULL
+        )
+    ''')
+
+    connection.commit()
+    connection.close()
+
+# the home page
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# the login page
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+# Add dog page
 @app.route('/add-dog')
 def add_dog():
     return render_template('add_dog.html')
 
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
