21506b3a9f311bbbf8e4d04544d951aa4483e93a
Samantha Kwenaeetile <74901613+SamanthaKwenaeetile@users.noreply.github.com>
2026-05-21 17:00:21 +0100

    Added SQLite form submission and dog profile saving

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -33,8 +33,27 @@ def login():
     return render_template('login.html')
 
 # Add dog page
-@app.route('/add-dog')
+@app.route('/add-dog', methods=['GET', 'POST'])
 def add_dog():
+
+    if request.method == 'POST':
+
+        dog_name = request.form['name']
+        dog_breed = request.form['breed']
+        dog_age = request.form['age']
+
+        connection = sqlite3.connect('database/dogtracker.db')
+
+        cursor = connection.cursor()
+
+        cursor.execute('''
+            INSERT INTO dogs (name, breed, age)
+            VALUES (?, ?, ?)
+        ''', (dog_name, dog_breed, dog_age))
+
+        connection.commit()
+        connection.close()
+
     return render_template('add_dog.html')
 
 if __name__ == '__main__':
